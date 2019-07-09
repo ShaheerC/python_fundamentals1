8f2f747839e8b0d511945b12fc05f7f06e26407e
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-07-09 14:36:50 -0400

    exercise2

---
+++ exercise2.py
@@ -0,0 +1,14 @@
+# Multiply 55 by 1.20 to add a 20% tip to the total
+print(55 * 1.20)
+
+# Turning an integer into a string using the str()
+print('testing' + str(7))
+
+# Use str and interpolation
+num = 45628 * 7839
+result = "The result is {}".format(num)
+print(result)
+print(type(result))
+
+#True
+print((10 < 20 and 30 < 20) or not(10 == 11)) 
