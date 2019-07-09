3d068305b8711c2911310db656bf8aa91a132a88
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-07-09 15:05:46 -0400

    exercise3 full

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,25 @@
+#Part One
 print("What is your name?")
+user_name = input()
+print("Hello, {}.".format(user_name))
+print("How old are you?")
+age = input()
+year_born = 2019 - int(age)
+print("So {}, you were born in {}".format(user_name, year_born))
+
+#Part Two
+secret_password = "please"
+print("What's the password?")
+password_attempt = input()
+
+correct_or_not = (password_attempt == secret_password)
+
+print("That's {}!".format(correct_or_not))
+
+#Part Three
+print("How many cookies have been eaten?")
+eaten = input()
+
+remaining_cookies = 12 - int(eaten)
+
+print("There are {} cookies left.".format(remaining_cookies))
